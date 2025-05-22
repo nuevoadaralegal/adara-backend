8dbfac96f1233a26f78e69d6601af52d560c4682
nuevoadaralegal <nuevoadaralegal@gmail.com>
2025-05-22 18:06:49 +0200

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 API_KEY = os.getenv("AIzaSyCk99OoL2D6XnmuXQDqLyq8EirQ1nuQ9-c")
-GEMINI_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-preview-05-20:generateContent?key={API_KEY}"
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
 @app.route("/api/conclusiones", methods=["POST"])
 def conclusiones():
